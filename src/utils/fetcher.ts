3cb45ef8b7d6d28f38734b6ecd20819c0a9e4cc8
Harsh Tuwar <tuwarharsh08@gmail.com>
2024-02-21 20:02:00 -0500

    testing revalidateTag

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -2,7 +2,13 @@ const { signal } = new AbortController();
 
 export const fetcher = (url: string) => fetch(url, { cache: 'no-store', signal }).then((r) => r.json()).catch((e) => console.log(e));
 
-export const aboutMeFetcher = (url: string) => fetch(url, { cache: 'no-store', signal }).then(async (res) => {
+export const aboutMeFetcher = (url: string) => fetch(url, {
+	cache: 'no-store',
+	signal,
+	next: {
+		tags: ['about_me']
+	}
+}).then(async (res) => {
 	const aboutMeRes = await res.json();
 
 	return {
