addeca834539348efa04647bc928624bb8c39957
Harsh Tuwar <tuwarharsh08@gmail.com>
2025-08-19 14:23:11 -0400

    added intergration with notion to fetch blogs

---
+++ types/global.types.ts
@@ -0,0 +1,64 @@
+
+type NotionMultiSelect = {
+	id: string
+	name: string
+	color: string
+}
+
+type NotionText = {
+	rich_text: {
+		plain_text: string
+	}[]
+}
+
+export type NotionPageProps = {
+	publishedAt: NotionText,
+	excerpt: NotionText,
+	slug: NotionText,
+	author: NotionText,
+	readTime: NotionText,
+	title: {
+		title: {
+			plain_text: string
+		}[]
+	},
+	status: {
+		id: string
+		status: {
+			id: string
+			name: string
+			color: string
+		}
+	},
+	heroImage: {
+		files: {
+			name: string
+			file: {
+				url: string
+			}
+		}[]
+	},
+	tags: {
+		multi_select: NotionMultiSelect[]
+	},
+	category: {
+		multi_select: NotionMultiSelect[]
+	},
+}
+
+export type GetAllBlogsResponse = {
+	id: string,
+	status: NotionMultiSelect,
+	heroImage: string,
+	heroImageName: string,
+	tags: NotionMultiSelect[],
+	publishedAt: string,
+	category: NotionMultiSelect[],
+	excerpt: string,
+	slug: string,
+	author: string,
+	readTime: string,
+	title: string,
+	dynamicUrl: string
+}
+
